e3e20872a66b765ee3e4dfdfb295a485057ff918
LocPhanFullstack <phananhloc03102001@gmail.com>
2024-12-17 22:09:59 +0700

    Complete Settings page

---
+++ src/app/(dashboard)/teacher/settings/page.tsx
@@ -0,0 +1,10 @@
+import { DashboardLayout } from "@/components/layout/dashboard";
+import { SettingsScreen } from "@/screens/Dashboard/Teacher/Settings";
+
+export default function TeacherSettings() {
+  return (
+    <DashboardLayout>
+      <SettingsScreen />
+    </DashboardLayout>
+  );
+}
---
+++ src/screens/Dashboard/Teacher/Settings/index.tsx
@@ -0,0 +1,12 @@
+import { SharedNotificationSettings } from "../../components";
+
+export const SettingsScreen = () => {
+  return (
+    <div className="w-3/5">
+      <SharedNotificationSettings
+        title="Teacher Settings"
+        subtitle="Manage your teacher notification settings"
+      />
+    </div>
+  );
+};
